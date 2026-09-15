10602ece819bac5dfac398745e7725e267a71eb8
uko05 <yosieyosie1@yahoo.co.jp>
2026-09-15 13:32:06 +0900

    バージョン画像修正

--- 02_Starrail/chara_data/starrail_chars.js
+++ 02_Starrail/chara_data/starrail_chars.js
@@ -14,6 +14,7 @@
 // ===== キャラクターデータ =====
 export const starrailChars = [
 
+  { icon: "aha.png",        path: "yuetu",    element: "ryoushi"    },
   { icon: "parl.png",       path: "yuetu",    element: "koori"    },
   { icon: "aben2.png",      path: "yuetu",    element: "ryoushi"  },
   { icon: "robin2.png",     path: "kioku",    element: "kaze"     },
